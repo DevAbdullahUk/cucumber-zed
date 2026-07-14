8e02fce0ce7ec9a2ed8dfe4764656df4f0795c38
abdullahjanjua <Abdullahjanjuauk@gmail.com>
2026-07-14 20:33:33 +0100

    Add outline.scm for Gherkin

---
+++ languages/gherkin/outline.scm
@@ -0,0 +1,11 @@
+(feature
+  name: (name_line) @name) @item
+
+(rule_header
+  name: (name_line) @name) @item
+
+(scenario
+  name: (name_line) @name) @item
+
+(background
+  name: (name_line) @name) @item
