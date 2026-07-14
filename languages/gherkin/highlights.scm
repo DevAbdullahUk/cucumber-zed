afbd227f5f886b90eacad7ab6aef07436ff00c2d
abdullahjanjua <Abdullahjanjuauk@gmail.com>
2026-07-14 20:31:07 +0100

    Add highlights.scm for Gherkin

---
+++ languages/gherkin/highlights.scm
@@ -0,0 +1,20 @@
+"Feature" @keyword
+"Rule" @keyword
+"Background" @keyword
+"Scenario" @keyword
+"Scenario Outline" @keyword
+"Examples" @keyword
+(step_keyword) @keyword
+
+(tag) @tag
+(comment) @comment
+(doc_string) @string
+
+"|" @punctuation.special
+":" @punctuation.delimiter
+
+(feature name: (name_line) @type)
+(rule_header name: (name_line) @type)
+(scenario name: (name_line) @type)
+(background name: (name_line) @type)
+(examples name: (name_line) @type)
